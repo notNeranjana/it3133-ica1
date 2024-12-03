146827fc86ef21869f03f8b077038aa2bf85d893
Neranjana Prasad <neranjanaone@gmail.com>
2024-12-03 17:48:25 +0530

    feat: update products grid

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,25 +1,19 @@
 export default function Cart() {
-    
-    return (
-        <div className="table-container">
-            <h4 className="card-title">Cart</h4>
-            <table>
-                <thead>
-
-                    <td>Product</td>
-                    <td>QTY</td>
-                    <td>Price</td>
-
-                </thead>
-                {
-                    
-                }
-                <tr>
-                    <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
-                </tr>
-            </table>
-           
-        </div>
-    );
+  return (
+    <div className="table-container">
+      <h4 className="card-title">Cart</h4>
+      <table>
+        <thead>
+          <td>Product</td>
+          <td>QTY</td>
+          <td>Price</td>
+        </thead>
+        {}
+        <tr>
+          <td>Grand Total : </td>
+          <td colSpan={2}>Grand Total</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,22 @@
-import '../assets/CSS/layout.css';
-export default function Product(){
-    
-    return(
-        <div className="grid-item">
+import "../assets/CSS/layout.css";
 
-            <div class="card">
-                <img  />
-                <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
-                    <div class="quantity-container">
-                        <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
-                    </div>
-                    <button class="card-button">Add to Cart</button>
-                </div>
-            </div>
+export default function Product({ key, product, setCart, setTotal }) {
+  return (
+    <div className="grid-item" key={key}>
+      <div class="card">
+        <img
+          src={require(`../assets/image/${product.img}`)}
+          alt={product.name}
+        />
+        <div class="card-body">
+          <h5 class="card-title">{product.name} Price:</h5>
+          <div class="quantity-container">
+            <label for="quantity">Quantity:</label>
+            <input type="number" id="quantity" name="quantity" />
+          </div>
+          <button class="card-button">Add to Cart</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
