d73ab61b4746fc14237fba321b3ffb1ebd7ef4ea
Neranjana Prasad <neranjanaone@gmail.com>
2024-12-03 17:53:05 +0530

    feat: implement add to card functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import "../assets/CSS/layout.css";
 
 export default function Product({ key, product, setCart, setTotal }) {
+  const [quantity, setQuantity] = useState(0);
+
+  const addToCart = () => {
+    if (quantity === 0) {
+      setCart((prev) => [
+        ...prev,
+        {
+          product: product.name,
+          quantity,
+          price: product.price,
+        },
+      ]);
+      setTotal((prevTotal) => prevTotal + product.price * quantity);
+    }
+  };
+
   return (
     <div className="grid-item" key={key}>
       <div class="card">
@@ -12,9 +29,19 @@ export default function Product({ key, product, setCart, setTotal }) {
           <h5 class="card-title">{product.name} Price:</h5>
           <div class="quantity-container">
             <label for="quantity">Quantity:</label>
-            <input type="number" id="quantity" name="quantity" />
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              value={quantity}
+              onChange={(e) => {
+                setQuantity(e.target.value);
+              }}
+            />
           </div>
-          <button class="card-button">Add to Cart</button>
+          <button class="card-button" onClick={addToCart}>
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
